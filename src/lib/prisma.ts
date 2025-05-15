9e6a62fc4150f60e3ea097577474a2245d70396c
Adınız <sezer.polat@outlook.com.tr>
2025-05-16 00:07:59 +0300

    fix: update database URL environment variable name

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,7 +8,7 @@ export const prisma = globalForPrisma.prisma ?? new PrismaClient({
   log: ['error'],
   datasources: {
     db: {
-      url: process.env.DATABASE_POSTGRES_URL
+      url: process.env.DATABASE_URL
     }
   }
 });
